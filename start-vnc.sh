1351305117737f33dc42a6cbd363ac73785cbb78
nihilia10 <159345273+nihilia10@users.noreply.github.com>
2025-08-31 20:09:41 -0500

    Feature/allow clipboard sharing (#3)
    * allow copy paste between host
    
    * updated readme

--- start-vnc.sh
+++ start-vnc.sh
@@ -13,10 +13,5 @@ echo "VNC server started at $RESOLUTION!"
 # Arranca noVNC (6080 -> 5901)
 echo "Starting noVNC server..."
 exec websockify --web=/usr/share/novnc 0.0.0.0:6080 localhost:5901
-# /usr/share/novnc/utils/novnc_proxy \
-#   --vnc localhost:5901 \
-#   --listen 0.0.0.0:6080 \
-#   --web /usr/share/novnc &
-
 NOVNC_PID=$!
 wait $NOVNC_PID
